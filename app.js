9b13e6b98fa6f9a0060b2d70c7f761a9acd00f92
khudoyor01 <nasima.sharipova1967@gmail.com>
2021-08-05 18:00:22 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let name = prompt("Isminggini kiriting");
+let sum1 = prompt("Do'llardagi pulinggizni kiriting");
+let sum2 = prompt("EUROdagi pulinggizni kiriting");
+let d = 9433.34;
+let e = 10354.03;
+let result = sum1 * d + sum2 * e;
+let fly = 8317488.6;
+function flew(jump) {
+  if (jump < 0) {
+    alert(`${name} manfiy yozmang`);
+  } else if ((jump >= 0, jump < fly)) {
+    alert(`${name}, ozgina sabr qilish kerak bo’lar ekan`);
+  } else {
+    alert(`Oq yo’l, ${name}!`);
+  }
+}
+flew(result);
